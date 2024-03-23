dd3bbedcb387ea279c94c18ffbc6d4f0695e0106
minkhantzaw <minkhantzaw@g.com>
2024-03-23 21:57:17 +0630

    setup nextauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from "@nextui-org/react";
+import * as action from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/Profile";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
-      <Button>Click Me</Button>
+      <form action={action.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={action.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed out </div>
+      )}
+
+      <Profile/>
     </div>
   );
 }
