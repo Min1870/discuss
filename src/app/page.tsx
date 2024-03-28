961c3ad56920ad9bfcccd6ed82656238cb80532f
minkhantzaw <minkhantzaw@g.com>
2024-03-28 11:45:35 +0630

    Finsh CreatePostFrom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Divider } from "@nextui-org/react";
 import TopicCreateForm from "@/components/topics/TopicCreateForm";
+import TopicLists from "@/components/topics/TopicLists";
 
 export default function Home() {
   return (
@@ -6,8 +8,11 @@ export default function Home() {
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top Posts</h1>
       </div>
-      <div className="">
+      <div className="border shadow py-3 px-2">
         <TopicCreateForm />
+        <Divider className="my-2" />
+        <h3 className="text-lg mb-2">Topics</h3>
+        <TopicLists />
       </div>
     </div>
   );
--- src/components/posts/PostCreateForm.tsx
+++ src/components/posts/PostCreateForm.tsx
@@ -5,47 +5,54 @@ import {
   Button,
   Textarea,
   Popover,
-  PopoverTrigger,
   PopoverContent,
+  PopoverTrigger,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
 import FormButton from "@/components/common/FormButton";
 
-const TopicCreateForm = () => {
-  const [formState, action] = useFormState(actions.createTopic, {
-    errors: {},
-  });
+interface PostCreateFormProps {
+  slug: string;
+}
+
+const PostCreateForm = ({ slug }: PostCreateFormProps) => {
+  const [formState, action] = useFormState(
+    actions.createPost.bind(null, slug),
+    {
+      errors: {},
+    }
+  );
   return (
     <Popover placement="left">
       <PopoverTrigger>
-        <Button color="primary">Create a Topic</Button>
+        <Button color="primary">Create a post</Button>
       </PopoverTrigger>
       <PopoverContent>
         <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
-            <h3 className="text-lg">Create a Topic</h3>
+            <h3 className="text-lg">Create a Post</h3>
             <Input
-              name="name"
-              label="Name"
+              name="title"
+              label="Title"
               labelPlacement="outside"
-              placeholder="Name"
-              isInvalid={!!formState.errors.name}
-              errorMessage={formState.errors.name?.join(", ")}
+              placeholder="Title"
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(", ")}
             />
             <Textarea
-              name="description"
-              label="Description"
+              name="content"
+              label="Content"
               labelPlacement="outside"
-              placeholder="Describe your topic"
-              isInvalid={!!formState.errors.description}
-              errorMessage={formState.errors.description?.join(", ")}
+              placeholder="Content"
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(", ")}
             />
             {formState.errors._form ? (
               <div className="p-2 bg-red-200 border border-red-400 rounded-md">
                 {formState.errors._form?.join(", ")}
               </div>
             ) : null}
-            <FormButton>Submit</FormButton>
+            <FormButton>Create Post</FormButton>
           </div>
         </form>
       </PopoverContent>
@@ -53,4 +60,4 @@ const TopicCreateForm = () => {
   );
 };
 
-export default TopicCreateForm;
+export default PostCreateForm;
