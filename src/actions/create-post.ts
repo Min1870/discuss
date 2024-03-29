4c60b18055cd696cdfa7525a6fe94d19210af616
minkhantzaw <minkhantzaw@g.com>
2024-03-29 20:04:50 +0630

    Complete PostShowPage

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 import { z } from "zod";
 import { auth } from "@/auth";
 import { db } from "@/db";
-import path from "@/path";
+import path from "@/path";  
 
 const createPostSchema = z.object({
   title: z.string().min(3),
